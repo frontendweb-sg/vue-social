db8d7d73a8e8ac75be00b6db41be1c9971fcee85
frontendweb-sg <frontendweb.sg@gmail.com>
2024-05-01 15:52:37 +0530

    add pinia,router and axios dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import './assets/main.css'
+
+const app = createApp(App)
+
+// global component
+
+// global directives
+
+// global provider
+
+// pinia
+const pinia = createPinia()
+app.use(pinia)
+
+// routes
+
+// app configuration
+
+// mount app
+app.mount('#app')
