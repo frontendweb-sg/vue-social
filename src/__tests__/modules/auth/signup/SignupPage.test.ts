ebbbb578f5c0e20153d4d2e5f0feb63acfe94ec6
frontendweb-sg <frontendweb.sg@gmail.com>
2024-05-01 16:55:21 +0530

    add __test__ folder

---
+++ src/__tests__/modules/auth/signup/SignupPage.test.ts
@@ -0,0 +1,10 @@
+import { describe, test, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import SignupPage from '@/modules/auth/signup/SignupPage.vue'
+
+describe('Signup component', () => {
+  test('Initial component loading', () => {
+    const wrapper = mount(SignupPage)
+    expect(wrapper.text()).toContain('Sign up page')
+  })
+})
